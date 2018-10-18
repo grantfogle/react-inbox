aea507e4f7dc8d2e51abaaac1919ab7c0e1cd246
grantfogle <grantjfogle@gmail.com>
2018-10-18 17:59:03 -0600

    adding labels

--- src/Message.js
+++ src/Message.js
@@ -12,7 +12,7 @@ class Messages extends Component {
 
     render() {
         return (
-            <div>
+            <>
                 {
                     <div className={`row message ${this.props.readStatus ? 'read' : 'unread'} ${this.props.checked ? 'selected' : ''}`}>
                         <div className="col-xs-1">
@@ -26,14 +26,43 @@ class Messages extends Component {
                             </div>
                         </div>
                         <div className="col-xs-11">
-                            <a className="" onClick={() => this.props.toggleRead(this.props.readStatus)}
-                                href="#"> {this.props.subject}</a>
-                        </div>
-                    </div>
+                            {/* <span className={`label label-warning ${this.props.label.personal ? '' : 'hidden'}`}>dev</span>
+                            <span className={`label label-warning ${this.props.label.personal ? '' : 'hidden'}`}>personal</span>
+                            <span className={`label label-warning ${this.props.label.personal ? '' : 'hidden'}`}>gschool</span> */}
+                            <a className="" onClick={() => this.props.toggleRead(this.props.readStatus)} href="#"> {this.props.subject}</a>
+                        </div >
+                    </div >
                 }
-            </div>
+            </>
         )
     }
 }
 
 export default Messages;
+
+//add this to message componenent
+// <div class="row message read">
+//   <div class="col-xs-1">
+//     <div class="row">
+//       <div class="col-xs-2">
+//         <input type="checkbox" />
+//       </div>
+//       <div class="col-xs-2">
+//         <i class="star fa fa-star"></i>
+//       </div>
+//     </div>
+//   </div>
+//   <div class="col-xs-11">
+//     <span class="label label-warning">dev</span>
+//     <span class="label label-warning">gschool</span>
+//     <a href="#">
+//       Here is some message text that has a bunch of stuff
+//     </a>
+//   </div>
+// </div>
+
+// <div class="row message-body">
+//   <div class="col-xs-11 col-xs-offset-1">
+//     This is the body of the message.
+//   </div>
+// </div>
--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,47 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
+import ComposeForm from './ComposeForm';
 
 class Toolbar extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
     render() {
         return (
-            <div class="row toolbar">
-                <div class="col-md-12">
-                    <p class="pull-right">
-                        <span class="badge badge">2</span>
-                        unread messages
-    </p>
+            <>
+                <div className="row toolbar">
+                    <div className="col-md-12">
+                        <p className="pull-right">
+                            <span className="badge badge">2</span>
+                            unread messages
+                    </p>
+                        <a className={`btn btn-danger`} onClick={this.props.toggleComposeMessage}>
+                            <i className={`fa ${this.props.showComposeMessage ? 'fa-minus' : 'fa-plus'}`}></i>
+                        </a>
 
-                    <a class="btn btn-danger">
-                        <i class="fa fa-plus"></i>
-                    </a>
+                        <button className={`btn btn-default`}>
+                            <i className="fa fa-minus-square-o"></i>
+                        </button>
 
-                    <button class="btn btn-default">
-                        <i class="fa fa-minus-square-o"></i>
-                    </button>
+                        <button className="btn btn-default">Mark As Read</button>
 
-                    <button class="btn btn-default">Mark As Read</button>
+                        <button className="btn btn-default">Mark As Unread</button>
 
-                    <button class="btn btn-default">Mark As Unread</button>
+                        <select className="form-control label-select">
+                            <option>Apply label</option>
+                            <option value="dev">dev</option>
+                            <option value="personal">personal</option>
+                            <option value="gschool">gschool</option>
+                        </select>
 
-                    <select class="form-control label-select">
-                        <option>Apply label</option>
-                        <option value="dev">dev</option>
-                        <option value="personal">personal</option>
-                        <option value="gschool">gschool</option>
-                    </select>
+                        <select className="form-control label-select">
+                            <option>Remove label</option>
+                            <option value="dev">dev</option>
+                            <option value="personal">personal</option>
+                            <option value="gschool">gschool</option>
+                        </select>
 
-                    <select class="form-control label-select">
-                        <option>Remove label</option>
-                        <option value="dev">dev</option>
-                        <option value="personal">personal</option>
-                        <option value="gschool">gschool</option>
-                    </select>
-
-                    <button class="btn btn-default">
-                        <i class="fa fa-trash-o"></i>
-                    </button>
+                        <button className="btn btn-default">
+                            <i className="fa fa-trash-o"></i>
+                        </button>
+                    </div>
                 </div>
-            </div>
+                <div className={this.props.showComposeMessage ? '' : 'hidden'}>
+                    <ComposeForm />
+                </div>
+            </>
         );
     }
 }

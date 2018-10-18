14c0080d8ba93f9dd0f9dcd5d4ab57aa36d3df59
grantfogle <grantjfogle@gmail.com>
2018-10-18 17:10:00 -0600

    updated toolbar and added workflow.md

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -4,37 +4,41 @@ import './App.css';
 class Toolbar extends Component {
     render() {
         return (
-            <div className="row toolbar">
-                <div className="col-md-12">
-                    <p className="pull-right">
-                        <span className="badge badge">2</span>
+            <div class="row toolbar">
+                <div class="col-md-12">
+                    <p class="pull-right">
+                        <span class="badge badge">2</span>
                         unread messages
-                    </p>
+    </p>
 
-                    <button className="btn btn-default">
-                        <i className="fa fa-check-square-o"></i>
+                    <a class="btn btn-danger">
+                        <i class="fa fa-plus"></i>
+                    </a>
+
+                    <button class="btn btn-default">
+                        <i class="fa fa-minus-square-o"></i>
                     </button>
 
-                    <button className="btn btn-default">Mark As Read</button>
+                    <button class="btn btn-default">Mark As Read</button>
 
-                    <button className="btn btn-default">Mark As Unread</button>
+                    <button class="btn btn-default">Mark As Unread</button>
 
-                    <select className="form-control label-select">
+                    <select class="form-control label-select">
                         <option>Apply label</option>
                         <option value="dev">dev</option>
                         <option value="personal">personal</option>
                         <option value="gschool">gschool</option>
                     </select>
 
-                    <select className="form-control label-select">
+                    <select class="form-control label-select">
                         <option>Remove label</option>
                         <option value="dev">dev</option>
                         <option value="personal">personal</option>
                         <option value="gschool">gschool</option>
                     </select>
 
-                    <button className="btn btn-default">
-                        <i className="fa fa-trash-o"></i>
+                    <button class="btn btn-default">
+                        <i class="fa fa-trash-o"></i>
                     </button>
                 </div>
             </div>
